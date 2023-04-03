4b05e17d8720878346aff38af09ca72521b31d87
eehhssaan <ehsani786@live.com>
2023-04-03 16:44:54 +0800

    update indx file (missing previously)

--- api/index.js
+++ api/index.js
@@ -1,17 +1,31 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 
 // internal imports
-const connectDB = require("../config/db");
-const userRoutes = require("../routes/userRoutes");
-const authenticateRoutes = require("../routes/authenticateRoutes");
-const adminRoutes = require("../routes/adminRoutes");
-const { isAuth } = require("../config/auth");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const { isAuth } = require("./config/auth");
+
+const cookieParser = require("cookie-parser");
 
 connectDB();
 const app = express();
 
 app.use(express.json({ limit: "4mb" }));
+
+//cookie parser
+app.use(cookieParser());
+
+// Set up session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //root route
 app.get("/", (req, res) => {
@@ -22,12 +36,10 @@ app.get("/welcome", isAuth, (req, res) => {
   res.status(200).send("Welcome 🙌 ");
 });
 
-app.use("/api/authenticate/", authenticateRoutes);
-
-app.use("/api/account/", isAuth, userRoutes);
+app.use("/api/user/", userRoutes);
 
 app.use("/api/admin/", adminRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3005;
 
 app.listen(PORT);
